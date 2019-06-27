6b4ed42ef225d89dbd7a833ccc44c893d5412aa2
Michael Reuter <mareuternh@gmail.com>
2019-06-27 15:06:40 -0700

    New layout for SkyPosition page.

--- src/components/SkyPosition.js
+++ src/components/SkyPosition.js
@@ -38,19 +38,66 @@ function SkyPosition(props) {
     return(
         <div>
         {!hasError ? (
-            <div>
-                <p>Altitude: {formatDoubleLabel(props.moonInfo.altitude, '°')}</p>
-                <p>Azimuth: {formatDoubleLabel(props.moonInfo.azimuth, '°')}</p>
-                <p>Right Ascension: {formatRightAscension(props.moonInfo.ra)}</p>
-                <p>Declination: {formatDoubleLabel(props.moonInfo.dec, '°')}</p>
-                <p>Rise Time: {riseTime}</p>
-                <p>Transit Time: {transitTime}</p>
-                <p>Set Time: {setTime}</p>
+            <div className="w3-container">
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Altitude:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{formatDoubleLabel(props.moonInfo.altitude, '°')}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Azimuth:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{formatDoubleLabel(props.moonInfo.azimuth, '°')}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Right Ascension:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{formatRightAscension(props.moonInfo.ra)}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Declination:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{formatDoubleLabel(props.moonInfo.dec, '°')}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Rise Time:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{riseTime}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Transit Time:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{transitTime}</p>
+                    </div>
+                </div>
+                <div className="w3-row">
+                    <div className="w3-half">
+                        <p>Set Time:</p>
+                    </div>
+                    <div className="w3-half">
+                        <p>{setTime}</p>
+                    </div>
+                </div>
             </div>
         ) : (
-          <div>
             <Error />
-          </div>
         )}
         </div>
     );
