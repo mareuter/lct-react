7f774e5355013df9ecc7e6d071d27a4550f88a65
Michael Reuter <mareuternh@gmail.com>
2019-09-02 21:34:13 -0700

    Create feature dialog.

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+import "./Feature.css";
 
 function Feature(props) {
-  return <li>{props.name}</li>;
+  return (
+    <li className="w3-bar">
+      <span className="w3-right info-button">
+        <FontAwesomeIcon icon={faInfoCircle} className="info" />
+      </span>
+      <div>
+        <span>{props.name}</span>
+      </div>
+    </li>
+  );
 }
 
 export default Feature;
--- src/components/FeatureList.js
+++ src/components/FeatureList.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./FeatureList.css";
 
 import Error from "./Error";
 import Feature from "./Feature";
 
+import { useModal } from "./Hooks";
+import FeatureDialog from "./FeatureDialog";
+
 import { createFeatureArray } from "../Features";
 
 function FeatureList(props) {
   const hasError = props.error;
   let featureArray = createFeatureArray(props.features);
+  let [isShowing, toggle] = useModal();
+  let [featureToShow, setFeatureToShow] = useState(null);
+
+  function showFeature(event) {
+    let target = event.target ? event.target : event.srcElement;
+    let span = null;
+    if (target.nodeName === "svg") {
+      span = target.parentElement;
+    }
+    if (target.nodeName === "path") {
+      span = target.parentElement.parentElement;
+    }
+    if (span === null) {
+      return;
+    }
+    let featureName = span.nextSibling.childNodes[0].innerHTML;
+    setFeatureToShow(featureArray.find(f => f.name === featureName));
+    toggle();
+  }
 
   return (
     <div>
@@ -18,9 +40,14 @@ function FeatureList(props) {
           <div className="w3-row w3-center">
             <h1>{props.title}</h1>
           </div>
-          <ul className="w3-ul">
+          <FeatureDialog
+            isShowing={isShowing}
+            hide={toggle}
+            feature={featureToShow}
+          />
+          <ul className="w3-ul" onClick={showFeature}>
             {featureArray.map(feature => (
-              <Feature key={feature.key} name={feature.name} type={feature.type} />
+              <Feature key={feature.key} name={feature.name} />
             ))}
           </ul>
         </div>
