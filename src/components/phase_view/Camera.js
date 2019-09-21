24328f3bcc1d21ecf908fd788646338530945595
Michael Reuter <mareuternh@gmail.com>
2019-09-20 19:52:49 -0700

    Fix orientation change issue and jumping view issue.

--- src/components/phase_view/Camera.js
+++ src/components/phase_view/Camera.js
@@ -1,33 +1,41 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import { useThree } from "react-three-fiber";
 
 const POSITION = [0, 0, 250];
 const BOX = 200;
 const BASE_ZOOM = 0.836;
+const HEIGHT_PERCENT_CHANGE = 10;
 
 function Camera(props) {
   let camera = useRef();
   let { setDefaultCamera } = useThree();
   let [aspect, setAspect] = useState(1);
   let [maxCameraZoomOut, setMaxCameraZoomOut] = useState(BASE_ZOOM);
+  let [currentHeight, setCurrentHeight] = useState(0);
 
-  function onResize() {
+  const resizeCallback = useCallback(() => {
     let div = document.getElementsByClassName("canvas")[0];
-    setAspect(div.clientWidth / div.clientHeight);
-    setMaxCameraZoomOut(
-      (BASE_ZOOM * Math.min(div.clientHeight, div.clientWidth)) / BOX
-    );
-    camera.current.updateProjectionMatrix();
-  }
+    let percentHeightChange =
+      (200 * Math.abs(currentHeight - div.clientHeight)) /
+      (currentHeight + div.clientHeight);
+    if (percentHeightChange > HEIGHT_PERCENT_CHANGE) {
+      setAspect(div.clientWidth / div.clientHeight);
+      setMaxCameraZoomOut(
+        (BASE_ZOOM * Math.min(div.clientHeight, div.clientWidth)) / BOX
+      );
+      camera.current.updateProjectionMatrix();
+      setCurrentHeight(div.clientHeight);
+    }
+  }, [currentHeight]);
 
   useEffect(() => void setDefaultCamera(camera.current), [setDefaultCamera]);
 
   useEffect(() => {
-    onResize();
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
-  }, []);
+    resizeCallback();
+    window.addEventListener("resize", resizeCallback);
+    return () => window.removeEventListener("resize", resizeCallback);
+  }, [resizeCallback]);
 
   return (
     <orthographicCamera
