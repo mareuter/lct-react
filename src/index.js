1b63456b0fb15e226b895d720faeb350bde11494
Michael Reuter <mareuternh@gmail.com>
2022-04-17 16:03:38 -0700

    Handle React 18 upgrade.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -7,13 +7,14 @@ import * as serviceWorker from "./serviceWorker";
 
 import { DateProvider } from "./DateContext";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <DateProvider>
       <App />
     </DateProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
