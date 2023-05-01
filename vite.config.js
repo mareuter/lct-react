f556fb479130036759aff74e32a9a31c4d69c4c7
Michael Reuter <mareuternh@gmail.com>
2023-04-30 20:46:18 -0700

    Changing to vitest.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-// vite.config.ts
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: [
+    // ...
+  ]
 })
