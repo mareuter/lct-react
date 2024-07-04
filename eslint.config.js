5a1829ac5269ca492071d10fb3a6e6cfe0000995
Michael Reuter <mareuternh@gmail.com>
2024-07-04 09:20:05 -0700

    Fixup flat config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,78 @@
-module.exports = {
-  extends: [
-    // By extending from a plugin config, we can get recommended rules without having to add them manually.
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    'eslint-config-prettier',
-  ],
-  settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
-      version: 'detect',
+import js from '@eslint/js';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['coverage/*', 'dist/*', '.prettierrc.js', 'env.d.ts', 'src/serviceWorker.js'],
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      eslintPluginReact,
+      eslintPluginImport,
+      eslintPluginJsxA11y,
+      tsEslintPlugin,
     },
-    // Tells eslint how to resolve imports
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    rules: {
+      'no-console': 1,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        // Tells eslint-plugin-react to automatically detect the version of React to use.
+        version: 'detect',
+      },
+      // Tells eslint how to resolve imports
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
       },
     },
   },
-  env: {
-    browser: true,
-    node: true,
-  },
-  rules: {
-    'no-console': 1,
-  },
-  ignorePatterns: ['node_modules/', 'dist/', '.prettierrc.js', '.eslintrc.js', 'env.d.ts', 'src/serviceWorker.js'],
-};
+];
+
+// module.exports = {
+//   extends: [
+//     // By extending from a plugin config, we can get recommended rules without having to add them manually.
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:import/recommended',
+//     'plugin:jsx-a11y/recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+//     // Make sure it's always the last config, so it gets the chance to override other configs.
+//     'eslint-config-prettier',
+//   ],
+//   settings: {
+//     react: {
+//       // Tells eslint-plugin-react to automatically detect the version of React to use.
+//       version: 'detect',
+//     },
+//     // Tells eslint how to resolve imports
+//     'import/resolver': {
+//       node: {
+//         paths: ['src'],
+//         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+//       },
+//     },
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//   },
+
+//   ignorePatterns: ['node_modules/', 'dist/', '.prettierrc.js', '.eslintrc.js', 'env.d.ts', 'src/serviceWorker.js'],
+// };
